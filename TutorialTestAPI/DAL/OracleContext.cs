dc56bca9d71f6ea51cd7ba51aa1837dc62b9d2f1
imai0421 <82428053+imai0421@users.noreply.github.com>
2021-04-14 19:15:54 +0900

    EntityFramework実装#1

---
+++ TutorialTestAPI/DAL/OracleContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TutorialTestAPI.Models;
+
+namespace TutorialTestAPI.DAL
+{
+    /// <summary>
+    /// ORACLE接続用のカスタムコンテキスト
+    /// </summary>
+    public class OracleContext: DbContext
+    {
+        /// <summary>
+        /// ヒーローテーブル
+        /// </summary>
+        public DbSet<Hero> Heroes { set; get; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public OracleContext() : base("name=OracleContext")
+        {
+            // NULL値検索を許容
+            Configuration.UseDatabaseNullSemantics = true;
+        }
+
+        /// <summary>
+        /// モデル生成時
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // スキーマ設定
+            modelBuilder.HasDefaultSchema("ADMIN");
+            // ＤＢ初期化
+            Database.SetInitializer<OracleContext>(new OracleInitializer());
+        }
+    }
+}
---
+++ TutorialTestAPI/Models/Map/HeroMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace TutorialTestAPI.Models.Map
+{
+    /// <summary>
+    /// ヒーローEntityマッピング
+    /// </summary>
+    public class HeroMap : EntityTypeConfiguration<Hero>
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public HeroMap()
+        {
+            this.ToTable("Heroes");
+            this.HasKey(Hero => Hero.Id);
+            this.Property(Hero => Hero.Name);
+        }
+    }
+}
